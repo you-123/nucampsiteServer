3dafd9a43dbd95ff771a84b6c3fd411b80c09f57
yusuf <yesufbushira9@gmail.com>
2020-11-04 15:33:16 -0600

    File Upload

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 const mongoose = require('mongoose');
+
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
@@ -62,7 +64,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
-
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
